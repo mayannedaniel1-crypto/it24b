356fc64b8232d0b50a024105a0d82d0d1ae97de8
mayannedaniel1-crypto <mayannedaniel1@gmail.com>
2025-09-05 12:21:02 +0800

    First test

--- factorials.js
+++ factorials.js
@@ -2,7 +2,7 @@ const readline=require("readline");
 
 const r1 = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    outcleput: process.stdout
 })
 
 console.log("Factorial Application");
